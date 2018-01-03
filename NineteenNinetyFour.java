3a8c7e233a1089eb27b8aa0ef5f0593d2acc822f
Daveu1983 <31164433+Daveu1983@users.noreply.github.com>
2018-01-03 22:22:31 +0000

    Add files via upload

---
+++ NineteenNinetyFour.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+public class NineteenNinetyFour extends NineteenNinetyThree {
+	public ArrayList<String> createTable() {
+		
+		ArrayList<String> nineteen94 = new ArrayList<String>(); {
+    
+		nineteen94.add("Manchester United");
+		nineteen94.add("Blackburn Rovers");
+		nineteen94.add("Newcastle");
+		nineteen94.add("Arsesnal");
+		nineteen94.add("Leeds United");
+		nineteen94.add("Wimbledon"); 
+		nineteen94.add("Sheffield Wednesday");
+		nineteen94.add("Liverpool"); 
+		nineteen94.add("QPR");
+		nineteen94.add("Aston Villa");
+		nineteen94.add("Coventry City");
+		nineteen94.add("Norwich City"); 
+		nineteen94.add("West Ham United"); 
+		nineteen94.add("Chelsea");
+		nineteen94.add("Tottenham Hotspur");
+		nineteen94.add("Manchester City");
+		nineteen94.add("Everton");
+		nineteen94.add("Southampton");
+		nineteen94.add("Ipswich Town"); 
+		nineteen94.add("Sheffied United");
+		nineteen94.add("Oldham Athletic");
+		nineteen94.add("Swindon Town");
+		return nineteen94;
+		}
+	}
+}
